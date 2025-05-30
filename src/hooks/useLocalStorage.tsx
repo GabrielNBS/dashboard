f3d8df04e6142963e1f4b7faf9a9b45417dac4da
Gabriel Nascimento <gabrielganbajf@gmail.com>
2025-05-30 11:35:03 -0300

    feat: implements a custom hook for saving to local storage

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+
+// Hook para armazenar e recuperar valores do localStorage
+export function useLocalStorage<T>(key: string, initialValue: T) {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try {
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.error('Error ao ler o valor do localStorage', error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value: T) => {
+    try {
+      setStoredValue(value);
+      window.localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.error('Error ao salvar o valor no localStorage', error);
+    }
+  };
+
+  return [storedValue, setValue] as const;
+}
