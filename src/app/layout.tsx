8f7d97dc2946aecb193a20dc3e5812d36fda0fa2
Gabriel Nascimento <gabrielganbajf@gmail.com>
2025-06-10 19:48:12 -0300

    feat: Adds ProductProvider to the RootLayout component
    Wraps the layout in a ProductProvider to manage product state across the application.
    
    This change enhances the component's ability to share product-related context with child components, improving data flow and state management.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import DashboardHeader from '@/components/organisms/DashboardHeader';
 import '../styles/global.css';
 import DashboardAside from '@/components/molecules/DashboardAside';
 import MainSection from '@/components/organisms/Main';
+import { ProductProvider } from '@/context/store';
 
 export default function RootLayout({
   children,
@@ -10,10 +11,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={`antialiased sm:grid sm:grid-cols-[15%_85%]`}>
-        <DashboardHeader />
-        <DashboardAside />
-        <MainSection>{children}</MainSection>
+      <body>
+        <ProductProvider>
+          <div className={`antialiased sm:grid sm:grid-cols-[15%_85%]`}>
+            <DashboardHeader />
+            <DashboardAside />
+            <MainSection>{children}</MainSection>
+          </div>
+        </ProductProvider>
       </body>
     </html>
   );
